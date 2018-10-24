445d4c99222894cf2e7b73c6f61971caff9b9cdf
tbroatch98 <thomas.broatch98@gmail.com>
2018-10-23 22:03:24 -0700

    facebook login

--- app/src/main/java/champagne86/com/classconnect/IndexActivity.java
+++ app/src/main/java/champagne86/com/classconnect/IndexActivity.java
@@ -1,13 +1,132 @@
 package champagne86.com.classconnect;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class IndexActivity extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class IndexActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //private static final String TAG = "FacebookLogin";
+
+
+    private FirebaseAuth mAuth;
+    private CallbackManager mCallbackManager;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        mAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+      //  Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                   //         Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                           Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        }
+
+                    }
+                });
+    }
+
+
+    public void signOut() {
+        mAuth.signOut();
+        LoginManager.getInstance().logOut();
+    }
+
+    /*private void updateUI(FirebaseUser user) {
+        if (user != null) {
+
+            findViewById(R.id.login_button).setVisibility(View.GONE);
+        } else {
+
+            findViewById(R.id.login_button).setVisibility(View.VISIBLE);
+        }
+    }*/
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+    //    if (i == R.id.buttonFacebookSignout) {
+    //        signOut();
+   //     }
+    }
+}
+
+    ////////////////////////////////////////////////////
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-    }
-}
+        LoginButton loginButton = findViewById(R.id.login_button);
+    };
+}*/
+
+
+
+    ////////////////////
+
+
+
--- app/src/main/java/champagne86/com/classconnect/MainActivity.java
+++ app/src/main/java/champagne86/com/classconnect/MainActivity.java
@@ -2,13 +2,22 @@ package champagne86.com.classconnect;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 3000;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
