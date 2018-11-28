a8d281e576ca1f7d65fddf5399d458dc52f912bd
tbroatch98 <thomas.broatch98@gmail.com>
2018-11-27 21:08:13 -0800

    some units tests

--- app/src/test/java/champagne86/com/classconnect/MessageTest.java
+++ app/src/test/java/champagne86/com/classconnect/MessageTest.java
@@ -1,5 +1,61 @@
+package champagne86.com.classconnect;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MessageTest {
 
+    @Test
+    public void getSenderDisplayName() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        assertTrue(testMsg1.getSenderDisplayName().equals("sendDisplayName"));
+    }
+
+    @Test
+    public void setSenderDisplayName() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        testMsg1.setSenderDisplayName("newSender");
+        assertTrue(testMsg1.getSenderDisplayName().equals("newSender"));
+    }
+
+
+    @Test
+    public void getSenderID() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        assertTrue(testMsg1.getSenderID().equals("sender"));
+    }
+
+    @Test
+    public void setSenderID() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        testMsg1.setSenderID("newSender");
+        assertTrue(testMsg1.getSenderID().equals("newSender"));
+    }
+
+    @Test
+    public void getMessage() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        assertTrue(testMsg1.getMessage().equals("abc"));
+    }
+
+    @Test
+    public void setMessage() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        testMsg1.setMessage("newMessage");
+        assertTrue(testMsg1.getMessage().equals("newMessage"));
+    }
+
+    @Test
+    public void getId() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        assertTrue(testMsg1.getId() == 0);
+    }
+
+    @Test
+    public void setId() {
+        Message testMsg1 = new Message(0, "abc", "sender", "sendDisplayName");
+        testMsg1.setId(2);
+        assertTrue(testMsg1.getId() == 2);
+    }
 }
--- app/src/test/java/champagne86/com/classconnect/UserTest.java
+++ app/src/test/java/champagne86/com/classconnect/UserTest.java
@@ -1,5 +1,87 @@
-import static org.junit.Assert.*;
+package champagne86.com.classconnect;
 
+import com.google.firebase.auth.FirebaseUser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
 public class UserTest {
+    static String SAMPLENAME = "Thomas";
+    @Mock
 
+    public FirebaseUser mUser;
+    public String uId = "sample";
+    public String userName;
+    public List<Classroom> classList = new ArrayList();
+
+    @InjectMocks
+    public User testUser;
+
+    @Test
+    public void getUid() {
+        when(mUser.getUid()).thenReturn("abcdef");
+        User sampleUser = new User(mUser, SAMPLENAME);
+        assertTrue(sampleUser.getUid().equals("abcdef"));
+    }
+
+
+    @Test
+    public void setUserName() {
+        when(mUser.getUid()).thenReturn("abcdef");
+        User sampleUser = new User(mUser, SAMPLENAME);
+        String NEWNAME = "newName";
+        sampleUser.setUserName(NEWNAME);
+        assertTrue(sampleUser.getUserName().equals(NEWNAME));
+    }
+
+    @Test
+    public void getUserName() {
+        when(mUser.getUid()).thenReturn("abcdef");
+        User sampleUser = new User(mUser, SAMPLENAME);
+        assertTrue(sampleUser.getUserName().equals(SAMPLENAME));
+    }
+
+    @Test
+    public void getClassList() {
+        assertTrue(testUser.getClassList().equals(classList));
+    }
+
+    @Test
+    public void addClassList() {
+        when(mUser.getUid()).thenReturn("abcdef");
+        User sampleUser = new User(mUser, SAMPLENAME);
+        List<Classroom> testList = new ArrayList();
+        sampleUser.addClassList(testList);
+
+    }
+
+    @Test
+    public void addClass() {
+        when(mUser.getUid()).thenReturn("abcdef");
+        User sampleUser = new User(mUser, SAMPLENAME);
+        Classroom sampleClass = new Classroom();
+        sampleUser.addClass(sampleClass);
+    }
+
+    @Test
+    public void numClasses() {
+        when(mUser.getUid()).thenReturn("abcdef");
+        User sampleUser = new User(mUser, SAMPLENAME);
+        assert (sampleUser.numClasses() == 0);
+    }
+
+    @Test
+    public void blankConstructor(){
+        User blankUser = new User();
+    }
 }
